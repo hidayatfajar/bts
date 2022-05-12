9cfeb1f0a454786b91ee46e1f4b0e794d9beee2f
Abel26 <abelhutapea87@gmail.com>
2022-05-12 17:23:01 +0700

    revisi

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import LandingPage from "./LandingPage/LandingPage";
 import SidebarAdmin from "./Admin/SidebarAdmin/SidebarAdmin.js";
-<<<<<<< HEAD
-// import Dashboard from "./Admin/Dashboard/Dashboard.jsx";
-// import Cart from "./Admin/Dashboard/Cart/Cart.jsx";
-=======
 import RPL from "./User/RPL/RPL1";
 
->>>>>>> 69a67734aadd9160087fca959e1e742a46e89dee
 export default class App extends Component {
   render() {
 
@@ -18,13 +13,9 @@ export default class App extends Component {
         <BrowserRouter>
           <Switch>
             <Route path="/" exact component={LandingPage} />
-<<<<<<< HEAD
-            <Route path="/admin/"  component={SidebarAdmin} />
-=======
             <Route path="/Admin" exact component={Admin} />
             <Route path="/SidebarAdmin"  component={SidebarAdmin} />
             <Route path="/rpl"  component={RPL} />
->>>>>>> 69a67734aadd9160087fca959e1e742a46e89dee
           </Switch>
         </BrowserRouter>
       </div>
