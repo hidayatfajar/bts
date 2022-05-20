5e6585f378cbd858c97fed05c27c97e93010dac6
ahmadfadhilah99 <ahmadfadhilah327@gmail.com>
2022-05-20 20:04:19 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import LandingPage from "./LandingPage/LandingPage";
-import SidebarAdmin from "./Admin/SidebarAdmin/SidebarAdmin.js";
+// import SidebarAdmin from "./Admin/SidebarAdmin/SidebarAdmin.js";
 import RPL from "./User/RPL/RPL1";
 
 export default class App extends Component {
@@ -13,8 +13,8 @@ export default class App extends Component {
         <BrowserRouter>
           <Switch>
             <Route path="/" exact component={LandingPage} />
-            <Route path="/Admin" exact component={Admin} />
-            <Route path="/SidebarAdmin"  component={SidebarAdmin} />
+            {/* <Route path="/Admin" exact component={Admin} /> */}
+            {/* <Route path="/SidebarAdmin"  component={SidebarAdmin} /> */}
             <Route path="/rpl"  component={RPL} />
           </Switch>
         </BrowserRouter>
