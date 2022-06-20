3ac13be117e998773262505dfb4b64ff32705b38
Fajar Hidayat <fajarnh67@gmail.com>
2022-06-20 14:55:21 +0700

    configured dynamic admin and user

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8000/';
+
 
 ReactDOM.render(
   <React.StrictMode>
