f7da2bce669c89a69b4e73119e5dc01c14a6833c
ahmadfadhilah99 <ahmadfadhilah327@gmail.com>
2022-06-29 16:28:15 +0700

    update page guru

--- src/User/components/Guru/Guru.jsx
+++ src/User/components/Guru/Guru.jsx
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 import { Container } from "react-bootstrap";
 import NavBar from "../../../LandingPage/NavBar/NavBar";
 import NavBarMagic from "../../../LandingPage/NavBarMagic/NavBarMagic";
+import GuruCardList from "./GuruCardList";
 import GuruFooter from "./GuruFooter";
 import GuruHeader from "./GuruHeader";
+import "../RPL.css";
 
 export default function Guru() {
+  const [gambar, setGambar] = useState([]);
+  const [siswa, setSiswa] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [modal, setModal] = useState(false);
+  const [siswaIndex, setSiswaIndex] = useState(1);
+
+  const sort = (a, b) => {
+    if (a.siswa_nama < b.siswa_nama) {
+      return -1;
+    }
+    if (a.siswa_nama > b.siswa_nama) {
+      return 1;
+    }
+  };
+
+  const filteredData = siswa.filter((el) => {
+    if (search === "") {
+      return el;
+    } else {
+      return el.siswa_nama.toLowerCase().includes(search);
+    }
+  });
+
+  const onModal = (e, siswa_id) => {
+    e.preventDefault();
+    setModal(!modal);
+    setSiswaIndex(siswa_id);
+  };
+
+  const getData = async () => {
+    const gambar = await axios.get("http://localhost:8000/kelas/gambar/1/");
+    setGambar(gambar.data.data);
+    const siswa = await axios.get("http://localhost:8000/siswa/kelas/1/");
+    setSiswa(siswa.data.data.sort(sort));
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      getData();
+      setLoading(false);
+    }, 3000);
+  }, [null]);
   return (
     <>
       <div className="bg-bts ">
@@ -22,9 +67,11 @@ export default function Guru() {
         <br />
         <Container>
           <GuruHeader />
-          {/* <WisudaHeader /> */}
           <br />
+          <GuruCardList siswa={siswa} search={search} loading={loading} />
         </Container>
+        <br />
+        <br />
         <GuruFooter />
       </div>
     </>
--- src/User/components/Guru/GuruCardList.jsx
+++ src/User/components/Guru/GuruCardList.jsx
@@ -1,104 +1,128 @@
-import React, {useEffect, useState} from "react";
-import { Link, useParams } from "react-router-dom";
-import bgBtn from "../../assets/bgBtn.png";
-import axios from 'axios'
+import React, { useState } from "react";
+import { Card, Row, Col, Container, Spinner } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTimes,
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "../RPL.css";
 
-export default function JurusanCardList() {
-  const { jurusanId } = useParams();
+export default function GuruCardList(props) {
+  const [modal, setModal] = useState(false);
+  const [siswaIndex, setSiswaIndex] = useState(1);
+  const siswa = props.siswa;
+  console.log(props.input);
 
-  const [kelas, setKelas] = useState([]);
+  const filteredData = siswa.filter((el) => {
+    if (props.search === "") {
+      return el;
+    } else {
+      return el.siswa_nama.toLowerCase().includes(props.search);
+    }
+  });
 
-  useEffect(() => {
-    getKelas();
-  }, [jurusanId]);
-
-  const getKelas = async () => {
-    const result = await axios.get(
-      `http://localhost:8000/kelas/jurusan/${jurusanId}`
-    );
-    setKelas(result.data.data);
+  const onModal = (e, siswa_id) => {
+    e.preventDefault();
+    setModal(!modal);
+    setSiswaIndex(siswa_id);
   };
+
   return (
-    <div>
-      {kelas ? (
-        kelas.length == 4 ? (
-          <div>
-            <div className="md:flex justify-between">
-              <Link to={`/kelas/${kelas[0].kelas_id}`} className="no-underline">
-                <div className="w-full mt-4 md:w-1/2 ">
-                  <div className="w-min mx-auto bg-white/30 backdrop-blur-xl px-3 pt-4 pb-16 rounded-md z-0 ">
-                    <img src={bgBtn} alt="" className="max-w-xs object-cover" />
-                    <h1 className="z-10 -mt-24 text-center text-5xl font-semibold tracking-wider text-white">
-                      {kelas[0].kelas_nama}
-                    </h1>
-                  </div>
-                </div>
-              </Link>
-              <Link to={`/kelas/${kelas[1].kelas_id}`} className="no-underline">
-                <div className="w-full mt-4 md:w-1/2 ">
-                  <div className="w-min mx-auto bg-white/30 backdrop-blur-xl px-3 pt-4 pb-16 rounded-md z-0 ">
-                    <img src={bgBtn} alt="" className="max-w-xs object-cover" />
-                    <h1 className="z-10 -mt-24 text-center text-5xl font-semibold tracking-wider text-white">
-                      {kelas[1].kelas_nama}
-                    </h1>
-                  </div>
-                </div>
-              </Link>
-            </div>
-            <div className="md:flex justify-center">
-              <Link to={`/kelas/${kelas[2].kelas_id}`} className="no-underline">
-                <div className="w-full mt-4 md:w-1/2 ">
-                  <div className="w-min mx-auto bg-white/30 backdrop-blur-xl px-3 pt-4 pb-16 rounded-md z-0 ">
-                    <img src={bgBtn} alt="" className="max-w-xs object-cover" />
-                    <h1 className="z-10 -mt-24 text-center text-5xl font-semibold tracking-wider text-white">
-                      {kelas[2].kelas_nama}
-                    </h1>
-                  </div>
-                </div>
-              </Link>
-              <Link to={`/kelas/${kelas[3].kelas_id}`} className="no-underline">
-                <div className="w-full mt-4 md:w-1/2 ">
-                  <div className="w-min mx-auto bg-white/30 backdrop-blur-xl px-3 pt-4 pb-16 rounded-md z-0">
-                    <img src={bgBtn} alt="" className="max-w-xs object-cover" />
-                    <h1 className="z-10 -mt-24 text-center text-5xl font-semibold tracking-wider text-white">
-                      {kelas[3].kelas_nama}
-                    </h1>
-                  </div>
-                </div>
-              </Link>
+    <>
+      {modal === true ? (
+        <div className="">
+          <div id="myModal" className="modal">
+            <div className="flex justify-center">
+              <div className="back mt-60 ">
+                <button
+                  className="bg-none md:bg-slate-600 sm:p-0 md:p-3 sm:w-auto md:w-14 sm:h-auto md:h-14 rounded-full"
+                  disabled={siswaIndex === 0 ? true : false}
+                  onClick={(e) => {
+                    setSiswaIndex(siswaIndex - 1);
+                  }}
+                >
+                  <FontAwesomeIcon
+                    className="text-3xl md:text-base"
+                    icon={faChevronLeft}
+                  />
+                </button>
+              </div>
+              {/* <!-- Modal content --> */}
+              <div className="modal-content md:mx-3 -mx-1  ">
+                <FontAwesomeIcon
+                  icon={faTimes}
+                  className="close"
+                  onClick={onModal}
+                />
+                {siswa[siswaIndex].siswa_gambar ? (
+                  <img
+                    src={
+                      "http://localhost:8000/public/images/ak/ak1/" +
+                      siswa[siswaIndex].siswa_gambar
+                    }
+                    alt={siswa[siswaIndex].siswa_nama}
+                  />
+                ) : (
+                  <Spinner animation="border" />
+                )}
+              </div>
+              <div className="next mt-60">
+                <button
+                  className="bg-none md:bg-slate-600 sm:p-0 md:p-3 sm:w-auto md:w-14 sm:h-auto md:h-14 rounded-full"
+                  onClick={(e) => {
+                    setSiswaIndex(siswaIndex + 1);
+                  }}
+                >
+                  <FontAwesomeIcon
+                    className="text-3xl md:text-base"
+                    icon={faChevronRight}
+                  />
+                </button>
+              </div>
             </div>
           </div>
-        ) : (
-          <div className="md:flex justify-between">
-            {kelas
-              ? kelas.map((kelas, index) => {
-                  return (
-                    <Link
-                      to={`/kelas/${kelas.kelas_id}`}
-                      className="no-underline"
-                      key={kelas.kelas_id}
-                    >
-                      <div className="w-full mt-4 md:w-1/3 ">
-                        <div className="w-min mx-auto bg-white/30 backdrop-blur-xl px-3 pt-4 pb-16 rounded-md z-0 ">
-                          <img
-                            src={bgBtn}
-                            alt=""
-                            className="max-w-xs object-cover"
-                          />
-                          <h1 className="z-10 -mt-24 text-center text-5xl font-semibold tracking-wider text-white">
-                            {kelas.kelas_nama}
-                          </h1>
-                        </div>
-                      </div>
-                    </Link>
-                  );
-                })
-              : null}
-          </div>
-        )
+        </div>
+      ) : null}
+      {props.loading ? (
+        <center>
+          <Spinner variant="success" animation="border" />
+        </center>
       ) : (
-        console.warn("tidak tersedia")
-      )}{" "}
-    </div>
+        <div className="md:grid grid-cols-4 space-y-4 md:space-y-0 gap-4 mx-auto">
+          {filteredData.map((siswa, index) => (
+            <div className="w-full pt-7" key={siswa.siswa_id}>
+              <Card className="xs:h-auto laptop:h-96 mx-auto my-0 p-1 rounded-xl">
+                <LazyLoadImage
+                  className="img-siswa"
+                  variant="top"
+                  src={
+                    "http://localhost:8000/public/images/ak/ak1/" +
+                    siswa.siswa_gambar
+                  }
+                  onClick={(e) => onModal(e, index)}
+                  loading="lazy"
+                />
+                <Card.Body className="text-black">
+                  {/* <h6 className="text-black font-bold text-lg tablet:text-base laptop:text-base tracking-wide">
+                    Nama
+                  </h6> */}
+                  <p className="text-black text-lg tablet:text-base laptop:text-base  font-bold tracking-wide ">
+                    {siswa.siswa_nama}
+                  </p>
+                  {/* <h6 className="text-black font-bold text-lg tablet:text-base laptop:text-base tracking-wide pt-2">
+                    Jabatan
+                  </h6> */}
+                  <p className="text-black text-lg tablet:text-base laptop:text-base  font-light tracking-wide -mt-2">
+                    Guru Produktif Akuntansi
+                  </p>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
