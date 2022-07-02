9f5ec04ff5f95635623f7ca974ae31f407f77cc5
Fajar Hidayat <fajarnh67@gmail.com>
2022-07-02 17:28:05 +0700

    Mengubah App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component} from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import LandingPage from "./LandingPage/LandingPage";
-import SidebarAdmin from "./Admin/SidebarAdmin/SidebarAdmin.js";
+import SidebarAdmin from "./components/Admin/SidebarAdmin";
 
 import Jurusan from "./User/components/Jurusan/Jurusan"
 import Kelas from "./User/components/Kelas/Kelas"
 import Wisuda from "./User/components/Wisuda/Wisuda";
 import Guru from "./User/components/Guru/Guru";
-import { Login } from "./Admin/SidebarAdmin/Login";
+import { Login } from "./components/Admin/Login";
 
 export default class App extends Component {
   render() {
@@ -19,7 +19,7 @@ export default class App extends Component {
             <Switch>
               <Route path="/" exact component={LandingPage} />
               {/* <Route path="/Admin" exact component={Admin} /> */}
-              <Route path="/login" component={Login} />
+              <Route path="/auth/login" component={Login} />
               <Route path="/admin" component={SidebarAdmin} />
               <Route path="/jurusan/:jurusanId" component={Jurusan} />
               <Route path="/kelas/:kelasId" component={Kelas} />
